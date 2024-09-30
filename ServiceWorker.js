92624b48b83cf720116c79c4dd03999bd8af1e2f
josh-hall-griffith <jhall.develop@gmail.com>
2024-09-30 15:54:09 +1000

    EDIT: updated the build number to see if it fixes caching

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "Griffith University - Experimental Games Lab-Tully360-0.2";
+const contentToCache = [
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/d58bb93d845b99d14cdc59feddd3498a.framework.js",
+    "Build/ac8aed0933f8b76dcc7f8465228420b5.data",
+    "Build/68ff2322e0f293c3f7ec84c981cc9c16.wasm",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
